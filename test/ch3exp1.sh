7bfb9c5bc60d4ecde5de9cc91b2121363bef106f
samlee <samlee2015jp@gmail.com>
2020-01-20 00:33:59 +0900

    add

--- test/ch3exp1.sh
+++ test/ch3exp1.sh
@@ -3,9 +3,9 @@ echo -n "Enter the name of an animal: "
 read ANIMAL
 echo -n "The $ANIMAL has "
 case $ANIMAL in
-	horse | dog | cat) echo -n "four";;
-	man | kangaroo ) echo -n "two";;
-        sam ) echo -n "two";;
+	horse | dog | cat | sam ) echo -n "four";;
+	man | kangaroo | sam ) echo -n "two";;
+        sam lee ) echo -n "two";;
 	*) echo -n "an unknown number of";;
 esac
 	echo " legs."
